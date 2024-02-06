dad82ecc98a23d03c232c33117e2117b2d6d5ba8
jibril <100243649+Jibril0912@users.noreply.github.com>
2024-02-06 05:19:26 +0000

    Add files
    Program was not originally written into github repository. This commit adds all files.

---
+++ finalSolveSudoku.py
@@ -0,0 +1,196 @@
+import time
+import copy
+
+PUZZLE_NUM = 1
+
+def getCorner(row: int, col: int) -> (int,int):
+    cornerCoord = lambda x : x // 3 * 3 
+    return cornerCoord(row), cornerCoord(col)
+
+def checkValid(place: int, puzzle: list[list[int]], row: int, col: int) -> bool:
+    # check row
+    for num in puzzle[row]:
+        if num == place:
+            return False
+    
+    # check columns
+    for rowCheck in puzzle:
+        if rowCheck[col] == place:
+            return False
+    
+    # check box
+    corner = getCorner(row,col) # get the top left corner for the box that coord is in 
+    for i in range(corner[0], corner[0]+3):
+        for j in range(corner[1],corner[1]+3):
+            if puzzle[i][j] == place:
+                return False
+    
+    return True
+
+def checkValidForInput(puzzle: list[list[int]], row: int, col: int) -> bool:
+    place = puzzle[row][col]
+    # check row
+    for countCol,num in enumerate(puzzle[row]):
+        if num == place and countCol != col:
+            return False
+    
+    # check columns
+    for countRow,rowCheck in enumerate(puzzle):
+        if rowCheck[col] == place and countRow != row:
+            return False
+    
+    # check box
+    corner = getCorner(row,col) # get the top left corner for the box that coord is in 
+    for i in range(corner[0], corner[0]+3):
+        for j in range(corner[1],corner[1]+3):
+            if puzzle[i][j] == place and i != row and j != col:
+                return False
+    
+    return True
+
+def increment(row: int,col: int) -> (int,int):
+    col += 1
+    if (col > 8):
+        row += 1
+        col = 0
+
+    return row,col
+
+def reduce(row: int, col: int) -> (int,int):
+    col -= 1
+    if (col < 0):
+        row -= 1
+        col = 8  
+
+    return row,col
+
+def goForward(row: int, col: int, zeroPuzzle: list[list[int]]) -> (int,int):
+    valid: bool = False
+    while not valid:
+        row,col = increment(row,col)
+        valid = True if (row == 9 and col == 0) or zeroPuzzle[row][col] == 0 else False
+    
+    return row,col
+
+def goBack(row: int, col: int, zeroPuzzle: list[list[int]]) -> (int,int):
+    valid: bool = False
+    while not valid:
+        row,col = reduce(row,col)
+        valid = True if zeroPuzzle[row][col] == 0 else False
+
+    return row,col
+
+def findValidBackSpace(row,col,puzzle,zeroPuzzle):
+    validBackSpace = False
+    while not validBackSpace:
+        validBackSpace = True
+        row,col = goBack(row,col,zeroPuzzle)
+        if puzzle[row][col] < 9:
+            curVal = puzzle[row][col] + 1
+            puzzle[row][col] = 0
+        else:
+            puzzle[row][col] = 0
+            validBackSpace = False  
+    return row,col,curVal
+
+
+def solveSudoku(puzzle: list[list[int]]) -> None:
+    zeroPuzzle = copy.deepcopy(puzzle)
+    curVal: int = 1
+    solved: bool = False
+    calNum: int = 0 # counts number of calculations
+    col: int = 0 # point to num in row
+    row: int = 0 # point to row
+    validBackSpace: bool = False
+
+    # go to first index which is zero
+    if puzzle[row][col] != 0: row,col = goForward(row,col,zeroPuzzle)
+
+    while not(solved):
+        validPlace = checkValid(curVal,puzzle,row,col)
+
+        if validPlace: 
+            puzzle[row][col] = curVal
+            curVal = 1
+            row,col = goForward(row,col,zeroPuzzle)
+        else:
+            if (curVal >= 9):
+                row,col,curVal = findValidBackSpace(row,col,puzzle,zeroPuzzle)
+            else:
+                curVal += 1
+        if (row == 9 and col == 0): solved = True
+        
+        calNum += 1
+        if (calNum > 900_000): raise Exception("ERROR: Time limit exceeded")
+
+    return puzzle,calNum
+
+def checkInputValid(puzzle: list[list[int]]) -> bool:
+    for rowNum,row in enumerate(puzzle):
+        for colNum,num in enumerate(row): 
+            if num != 0:
+                if checkValidForInput(puzzle,rowNum,colNum) == False or num > 9 or num < 0:
+                    return False
+    return True 
+
+def main():
+    if PUZZLE_NUM == 1:
+        puzzle = [
+            [0,0,9, 0,0,0, 0,1,5],
+            [5,0,0, 4,0,9, 7,0,0],
+            [4,7,3, 5,6,1, 9,0,0],
+            [0,0,0, 7,4,0, 0,9,6],
+            [0,0,0, 0,0,0, 0,8,0],
+            [0,0,4, 8,3,0, 1,5,0],
+            [1,3,5, 9,0,0, 0,0,2],
+            [0,0,6, 2,5,7, 0,3,0],
+            [7,2,0, 0,1,0, 0,0,9]
+        ]
+    elif PUZZLE_NUM == 2:
+        puzzle = [
+            [2,6,0,3,0,0,0,1,0],
+            [5,8,0,4,0,0,7,0,0],
+            [0,0,0,0,6,0,0,2,8],
+            [0,0,0,8,3,0,0,0,7],
+            [0,1,2,7,0,5,3,0,0],
+            [0,5,0,0,0,0,0,0,0],
+            [0,4,6,0,0,0,0,0,1],
+            [7,0,0,0,0,0,0,4,0],
+            [0,3,5,0,0,0,6,0,0]
+        ]
+    else:
+        puzzle = [
+            [0,7,0,0,0,6,0,0,5],
+            [0,4,0,8,0,0,0,0,0],
+            [8,0,5,0,0,1,0,3,0],
+            [9,0,6,1,0,0,0,0,4],
+            [0,0,0,0,0,3,2,0,0],
+            [0,8,0,0,0,0,0,0,0],
+            [5,0,1,9,0,0,0,0,6],
+            [0,0,7,0,0,0,0,0,0],
+            [0,0,0,0,6,0,0,4,0]
+        ]
+
+    startTime: time = time.time()
+    puzzle,calNum = solveSudoku(puzzle)
+    endTime: time = time.time()
+
+    for row in puzzle: print(row)
+    print(f"\nFINAL TIME: {endTime - startTime}")
+    print(f"NUM OF CALCULATIONS: {calNum:,}")
+
+
+if __name__ == "__main__":
+    #main()
+    puzzle = [
+            [0,7,0,0,0,6,0,0,5],
+            [0,4,0,8,0,0,0,0,0],
+            [8,0,5,0,0,1,0,3,0],
+            [9,0,6,1,0,0,0,0,4],
+            [0,0,0,0,0,3,2,0,0],
+            [0,8,0,0,0,0,0,0,0],
+            [5,0,1,9,0,0,0,0,6],
+            [0,0,7,0,0,0,0,0,0],
+            [0,0,0,0,6,0,0,4,0]
+        ]
+    print(checkInputValid(puzzle))
---
+++ sudokuGUI.py
@@ -0,0 +1,169 @@
+import customtkinter as ctk
+import finalSolveSudoku as solveSudoku
+import tkinter as tk
+import time
+import copy
+
+# CONSTANTS
+FONT = ("Helvetica",14)
+FONT_BOLD = ("Calibri bold",16)
+
+# SUBROUTINES
+def placeCol(master,col,row):
+    canvas = tk.Canvas(master,width=5,height=75,bg="black", highlightthickness=0)
+    canvas.grid(column=col,row=row)
+
+def placeRow(master,row,col):
+    canvas = tk.Canvas(master,width=75,height=5,bg="black", highlightthickness=0)
+    canvas.grid(column=col,row=row,rowspan=1)
+
+def fillGap(master,row,col):
+    canvas = tk.Canvas(master,width=5,height=5,bg="black", highlightthickness=0)
+    canvas.grid(column=col,row=row)
+
+# CLASSES
+class AnswerGrid(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.grid(column=3,row=1,sticky="e")
+        aRow = 0
+        aCol = 0
+        self.boxes = []
+
+        for rowNum in range(11):
+            if (rowNum == 3 or rowNum == 7):
+                for i in range(11):
+                    if i != 3 and i != 7:
+                        placeRow(self,rowNum,i)
+                    else:
+                        fillGap(self,rowNum,i)
+            else:
+                aCol = 0
+                boxRow = []
+                for columnNum in range(11):
+                    if columnNum == 3 or columnNum == 7:
+                        placeCol(self,columnNum,rowNum)
+                    else:
+                        box = ctk.CTkLabel(master=self,text="",width=75,height=75)
+                        box.grid(row=rowNum,column=columnNum,sticky="e")
+                        boxRow.append(box)
+                        aCol += 1
+                aRow += 1
+                self.boxes.append(boxRow)
+    
+    def newSolve(self,puzzle: list[list[int]],zeroPuzzle:list[list[int]] = []):
+        for aRow,boxRow in enumerate(self.boxes):
+            for aCol,box in enumerate(boxRow):
+                box.configure(text=puzzle[aRow][aCol],text_color="grey" if zeroPuzzle[aRow][aCol] != 0 else "white",font=FONT) if puzzle != [] else box.configure(text="")
+
+
+class SudokuGrid(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.grid(column=1,row=1)
+        self.dimen = 75
+        self.entries = []
+
+        for rowNum in range(11):
+            if (rowNum == 3 or rowNum == 7):
+                for i in range(11):
+                    if i != 3 and i != 7:
+                        placeRow(self,rowNum,i)
+                    else:
+                        fillGap(self,rowNum,i)
+            else:
+                rowEntries = []
+                for colNum in range(11):
+                    if colNum == 3 or colNum == 7:
+                        placeCol(self,colNum,rowNum)
+                    else:
+                        entry = ctk.CTkEntry(self, placeholder_text="", width=self.dimen, height=self.dimen, corner_radius=0)
+                        entry.grid(column=colNum, row=rowNum)
+                        rowEntries.append(entry)
+            
+                self.entries.append(rowEntries)
+
+    def solvePuzzle(self,answerGrid: list[list[int]],status: ctk.CTkLabel,calculations: ctk.CTkLabel):
+        puzzle: list[list[int]] = []
+
+        for entry_row in self.entries:
+            row = []
+            for entry in entry_row:
+                if entry.get() != "" and entry.get() != 0:
+                    row.append(int(entry.get()))
+                else:
+                    row.append(0)
+            puzzle.append(row)
+
+        zeroPuzzle = copy.deepcopy(puzzle)
+
+        if solveSudoku.checkInputValid(puzzle) == True:
+            # SOLVE PUZZLE
+            startTime: time = time.time()
+            puzzle,calNum = solveSudoku.solveSudoku(puzzle)
+            endTime: time = time.time()
+
+            # UPDATE STATUS MESSAGES
+            status.configure(text=f"Solved in {round(endTime-startTime,6)} seconds")  
+            calculations.configure(text=f"Calculations: {calNum:,}")
+            
+            # UPDATE ANSWER GRID
+            answerGrid.newSolve(puzzle,zeroPuzzle)
+        else:
+            puzzle = []
+            status.configure(text="Invalid Input - Unsolvable Puzzle")
+            calculations.configure(text="")
+            answerGrid.newSolve(puzzle)
+
+class Blank(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master,width=20,fg_color="transparent")
+        self.grid(column=2,row=1)
+        
+# MAIN FRAME CLASS
+class MainFrame(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master)
+        self.pack(padx=60,pady=30, fill="both",expand = True)
+
+        # FRAMES
+        self.sudokuGrid = SudokuGrid(self)
+        self.blank = Blank(self)
+        self.answerGrid = AnswerGrid(self)
+
+        # LABELS AND BUTTONS
+        # Empty label
+        self.empty1 = ctk.CTkLabel(self, text="")
+        self.empty1.grid(column=1,row=11)
+
+        # Status labels
+        self.status = ctk.CTkLabel(master=self,text="",width=50,font=FONT_BOLD)
+        self.status.grid(column=3,row=12)
+        
+        self.calculations = ctk.CTkLabel(master=self,text="",width=50,font=FONT)
+        self.calculations.grid(column=3,row=13)
+
+        # Solve Button
+        self.submit = ctk.CTkButton(master=self, text="Solve", width = 150, font=FONT_BOLD,command=lambda: self.sudokuGrid.solvePuzzle(self.answerGrid,self.status,self.calculations))
+        self.submit.grid(column=1, row=12)
+
+# MAIN APP CLASS
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1510x880")
+        self.title("SUDOKU SOLVER - BY JAYツ")
+        self.resizable(False,False)
+
+        self.frame = MainFrame(self)
+
+# MAIN
+def main():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("dark-blue")
+    app = App()
+    app.iconbitmap("sudokuIcon.ico")
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
